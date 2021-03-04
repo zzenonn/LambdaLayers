a0f6fe049d14ad13fd5799f9eb4594f45affb6bb
zzenonn <miguel.saavedra@obf.ateneo.edu>
2021-03-04 03:49:29 +0000

    Add build layers script

---
+++ builld-layers.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -x
+
+set -e
+if [ "$#" -lt 1 ]
+then
+echo  "Please specify the S3 bucket you will be uploading to."
+exit
+else
+echo -e "\c"
+fi
+
+bucket_name=$1
+
+for layer in ./python/* #$(ls ./python)
+do
+
+mkdir -p $layer/python/lib/python3.8/site-packages/
+pip3.8 install -t $layer/python/lib/python3.8/site-packages/ -r $layer/requirements.txt
+
+done
+
+aws cloudformation package --template-file template.yaml --s3-bucket $bucket_name --s3-prefix layers > packaged.yaml
+aws cloudformation deploy --stack-name LambdaLayers --template ./packaged.yaml
+rm packaged.yaml
